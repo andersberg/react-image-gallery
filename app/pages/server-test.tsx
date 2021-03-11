8deca6778b3cad19dce715036bca15352a997fd5
Anders Berg <16289789+andersberg@users.noreply.github.com>
2021-03-11 22:44:19 +0100

    Improve error handling

--- app/pages/server-test.tsx
+++ app/pages/server-test.tsx
@@ -2,9 +2,18 @@ import { GetStaticProps, GetStaticPropsResult } from "next";
 
 type ServerTestProps = {
 	message: string;
+	error?: string;
 };
 
 function ServerTest(props: ServerTestProps) {
+	if (props.error) {
+		return (
+			<main>
+				<h1>Error</h1>
+				<code>{props.error}</code>
+			</main>
+		);
+	}
 	return (
 		<main>
 			<h1>Server Test</h1>
@@ -16,16 +25,26 @@ function ServerTest(props: ServerTestProps) {
 export const getStaticProps: GetStaticProps = async (
 	context
 ): Promise<GetStaticPropsResult<ServerTestProps>> => {
-	const response = await fetch("http://127.0.0.1:8080/ping");
-	const message = await response.text();
+	try {
+		const response = await fetch("http://127.0.0.1:8080/ping");
+		const message = await response.text();
+		console.log(message);
 
-	console.log(message);
+		return {
+			props: {
+				message,
+			},
+		};
+	} catch (error) {
+		console.error(error);
 
-	return {
-		props: {
-			message,
-		},
-	};
+		return {
+			props: {
+				error: JSON.stringify(error, null, 2),
+				message: JSON.stringify(error),
+			},
+		};
+	}
 };
 
 export default ServerTest;
