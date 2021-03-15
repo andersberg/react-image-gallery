60441c6f5c1154d09bdc9087a11916726e7587e6
Anders Berg <16289789+andersberg@users.noreply.github.com>
2021-03-15 10:00:00 +0100

    Serve images from server

--- app/pages/api/images/[image].ts
+++ app/pages/api/images/[image].ts
@@ -1,20 +1,33 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { readFile } from "fs/promises";
-import { resolve } from "path";
 
 export default async function imageHandler(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
 	const { image } = req.query;
-	try {
-		const file = resolve(process.cwd(), "../static", image as string);
-		const data = await readFile(file);
+	const filename = Array.isArray(image) ? image[0] : image;
 
-		return res.send(data);
+	try {
+		const { body, status } = await fetch(
+			`http://127.0.0.1:8080/images/${filename}`
+		);
+
+		if (status === 200) {
+			return res.send(body);
+		}
+
+		// Set Content Type to string for all errors.
+		res.setHeader("Content-Type", "text");
+
+		if (status === 404) {
+			return res.status(404).end("Not found");
+		}
+
+		return res.status(status).end("Unknown server error");
 	} catch (error) {
 		console.error(error);
 
+		res.setHeader("Content-Type", "text");
 		return res.status(500).send(JSON.stringify(error, null, 2));
 	}
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,44 @@
 import fastify from "fastify";
+import { readFile } from "fs/promises";
+import { resolve } from "path";
 
-const server = fastify();
+const server = fastify({
+	logger: true,
+});
 
 server.get("/ping", async (request, reply) => {
 	return "pong\n";
 });
+
+const options = {};
+
+type RequestParams = {
+	filename: string;
+};
+// TODO:
+// * https://stackoverflow.com/questions/5823722/how-to-serve-an-image-using-nodejs
+
+server.get<{ Params: RequestParams }>(
+	"/images/:filename",
+	async (request, response) => {
+		server.log.info(`Request params: ${JSON.stringify(request.params)}`);
+
+		const { filename } = request.params;
+		try {
+			const file = resolve(process.cwd(), "../static", filename);
+			const data = await readFile(file);
+			return response.send(data);
+		} catch (error) {
+			if (error.code === "ENOENT") {
+				server.log.error(JSON.stringify(error, null, 2));
+				return response.status(404).send(error.msg);
+			}
+
+			server.log.error(JSON.stringify(error, null, 2));
+			return response.status(500).send(error.msg);
+		}
+	}
+);
 
 server.listen(8080, (err, address) => {
 	if (err) {
